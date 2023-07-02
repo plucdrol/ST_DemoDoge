93989abc5f761b38819003080b75add49ad2c2fd
P-L <plucdrol@gmail.com>
2023-07-02 01:43:44 +0000

    canvas

---
+++ src/components/SpaceTraders.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+
+const getPlayground = () => {
+    console.log('get playground')
+}
+
+</script>
+
+<template>
+  <div class="header">
+    <h1>SPACE TRADERS</h1>
+  </div>
+  <div class="api-stations">
+    <div class="playground">
+      <h1>Get Playground</h1>
+      <button @click="getPlayground">Get Playground</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.header {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    min-height: 10vh;
+}
+.stations {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    min-height: 100vh;
+}
+</style>
---
+++ src/views/SpaceTradersView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import SpaceTraders from '@/components/SpaceTraders.vue';
+</script>
+
+<template>
+    <SpaceTraders />
+</template>
