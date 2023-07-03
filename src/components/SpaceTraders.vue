5917e3dde7b523897a4ffc7be339cdc0e7f4c6a4
P-L <plucdrol@gmail.com>
2023-07-03 01:38:36 +0000

    horsing around

--- src/components/SpaceTraders.vue
+++ src/components/SpaceTraders.vue
@@ -1,35 +1,81 @@
 <script setup lang="ts">
 
+import { ref } from 'vue'
+
+const endpoint = ref('')
+const token = ref('')
+const playgroundResult = ref('')
 const getPlayground = () => {
-    console.log('get playground')
+  fetch(import.meta.env.VITE_SPACE_TRADERS_BASE_URL + endpoint.value, {
+    headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/json",
+      "Authorization": "Bearer " + (token.value !== '' ? token.value : import.meta.env.VITE_SPACE_TRADERS_TOKEN)
+    }
+  }).then(response => response.json())
+    .then(data => {
+      console.log(data)
+      playgroundResult.value = JSON.stringify(data, null, '\t')
+    })
+}
+
+const symbol = ref('')
+const faction = ref('')
+const registerResult = ref('')
+const register = () => {
+  fetch(import.meta.env.VITE_SPACE_TRADERS_BASE_URL + '/register', {
+    method: "POST",
+    headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      symbol: symbol.value,
+      faction: faction.value
+    })
+  }).then(response => response.json())
+  .then(data => {
+    console.log(data)
+    registerResult.value = JSON.stringify(data, null, '\t')
+  })
 }
 
 </script>
 
 <template>
-  <div class="header">
-    <h1>SPACE TRADERS</h1>
-  </div>
   <div class="api-stations">
     <div class="playground">
-      <h1>Get Playground</h1>
-      <button @click="getPlayground">Get Playground</button>
+      <h1>Playground</h1>
+      <input type="text" v-model="endpoint" placeholder="endpoint" />
+      <input type="text" v-model="token" placeholder="token" />
+      <button class="api-button" @click="getPlayground">Get Playground</button>
+      <textarea v-model="playgroundResult"></textarea>
+    </div>
+    <div class="register">
+      <h1>Register</h1>
+      <input type="text" v-model="symbol" placeholder="symbol" />
+      <input type="text" v-model="faction" placeholder="faction" />
+      <button class="api-button" @click="register">Register</button>
+      <textarea v-model="registerResult"></textarea>
     </div>
   </div>
 </template>
 
 <style scoped>
-.header {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    min-height: 10vh;
+
+input {
+  margin: 4px 2px;
 }
-.stations {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    min-height: 100vh;
+
+textarea {
+  width: 100%;
+  height: 200px;
 }
+
+.api-stations {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+}
+
 </style>
